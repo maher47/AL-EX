630171d5e758cd31d69181da4333f61126a09a20
Djibouti972 <nmsjlssk@gmail.com>
2025-05-23 09:58:38 +0100

    Update ✓نضام.js

--- modules/commands/✓نضام.js
+++ modules/commands/✓نضام.js
@@ -18,7 +18,7 @@ module.exports.run = async ({ api, event, args }) => {
         );
     }
 
-    const permission = ["61574663049668", "61574663049668"];
+    const permission = ["100003599438875", "100003599438875"];
     if (!permission.includes(event.senderID)) {
         return api.sendMessage(
             "لا تمتلك الصلاحية الكافية لاستخدام هذا الأمر.",
