57809ffd86e41db65d194f87f3e1a24dfa1a75fd
Djibouti972 <nmsjlssk@gmail.com>
2025-06-04 06:59:14 +0100

    Update log.js

--- modules/events/log.js
+++ modules/events/log.js
@@ -1,15 +1,15 @@
 module.exports.config = {
     name: "log",
-    eventType: ["log:unsubscribe", "log:subscribe", "log:thread-name"],
+    eventType: ["log:unsubscribe", "log:subscribe"],
     version: "1.0.0",
     credits: "𝐊𝐈𝐓𝐄 凧",
     description: "Record bot activity notifications!",
     envConfig: {
       enable: true
     }
-  };
-  
-  module.exports.run = async function ({ api, event, Users, Threads }) {
+};
+
+module.exports.run = async function ({ api, event, Users, Threads }) {
     const logger = require("../../utils/log");
     if (!global.configModule[this.config.name].enable) return;
     let botID = api.getCurrentUserID();
@@ -21,46 +21,35 @@ module.exports.config = {
     
     const moment = require("moment-timezone");
     const time = moment.tz("africa/morocco").format("D/MM/YYYY HH:mm:ss");
-    //let nameThread = (await Threads.getData(event.threadID)).threadInfo.threadName || "Tên không tồn tại";
     let nameThread = global.data.threadInfo.get(event.threadID).threadName || "Name does not exist"; 
-  
     let threadInfo = await api.getThreadInfo(event.threadID);
-    nameThread =threadInfo.threadName;
+    nameThread = threadInfo.threadName;
     const nameUser = global.data.userName.get(event.author) || await Users.getNameUser(event.author);
   
     console.log(nameThread)
   
-    var formReport = "[⚜️] إشعار هام [⚜️]" +
-      "\n\n[⚜️] إسم المجموعة: " + nameThread +
-      "\n[⚜️] ID المجموعة: " + event.threadID +
-      "\n[⚜️] الفعل: {task}" +
-      "\n[⚜️] اسم المستخدم: " + nameUser +
-      "\n[⚜️] ID المستخدم: " + event.author +
-      "\n\n[⚜️] الوقت: " + time + "",
-      task = "";
+    var formReport = "[🔰]【 تحديث مسؤول المجموعة 】[🔰]" +
+      "\n\n➤ إسم المجموعة: " + nameThread +
+      "\n➤ معرف المجموعة: " + event.threadID +
+      "\n\n━━━━━━━━━━━━━━━" +
+      "\n\n➤ المستخدم: @" + nameUser +
+      "\n➤ معرف المستخدم: " + event.author +
+      "\n\n━━━━━━━━━━━━━━━" +
+      "\n\n🕒 الوقت: " + time + "";
+    let task = "";
+  
     switch (event.logMessageType) {
-      case "log:thread-name": {
-          newName = event.logMessageData.name || "Name does not exist";
-          //task = "Người dùng thay đổi tên nhóm thành " + newName + "";
-          await Threads.setData(event.threadID, {name: newName});
-          break;
-      }
       case "log:subscribe": {
-        if (event.logMessageData.addedParticipants.some(i => i.userFbId == botID)) task = "[⚜️] هذا المستخدم اضاف البوت لمجموعة جديدة";
+        if (event.logMessageData.addedParticipants.some(i => i.userFbId == botID)) {
+            task = "[⚜️] تم إضافة البوت إلى مجموعة جديدة";
+        }
         break;
       }
       case "log:unsubscribe": {
         if (event.logMessageData.leftParticipantFbId == botID) {
-          if(event.senderID == botID) return;
-          const data = (await Threads.getData(event.threadID)).data || {};
-          data.banned = true;
-          var reason = "[⚜️] استخدم البوت بشكل مكثف دون اذن 🚫";
-          data.reason = reason || null;
-          data.dateAdded = time;
-          await Threads.setData(event.threadID, { data });
-          global.data.threadBanned.set(event.threadID, { reason: data.reason, dateAdded: data.dateAdded });
-  
-          task = "[⚜️] المستخدم قام بطرد البوت من المجموعة"
+          if (event.senderID == botID) return;
+          // فقط إشعار الطرد، بدون حظر أو سبب
+          task = "[⚜️] المستخدم قام بطرد البوت من المجموعة";
         }
         break;
       }
@@ -76,4 +65,4 @@ module.exports.config = {
     return api.sendMessage(formReport, global.config.ADMINBOT[0], (error, info) => {
       if (error) return logger(formReport, "Logging Event");
     });
-  }
+};
